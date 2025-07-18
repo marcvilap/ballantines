bebdad59b32e98af5ae941832aa4264a63405635
Marc Vila <marc@mhou.es>
2025-07-18 16:20:15 +0200

    second

--- astro.config.ts
+++ astro.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+	//base: 'bc/experiencias10',
 	devToolbar: { enabled: false },
 	vite: { plugins: [tailwindcss()] },
 })
